5bebc28b5df0dd1cdd703531c124604ce0a275f3
Parchuk <sergeyparchuk1@gmail.com>
2020-01-31 18:42:51 +0200

    add selected

---
+++ src/blocks/modules/price/price.js
@@ -0,0 +1,14 @@
+
+$.widget( 'app.selectmenu', $.ui.selectmenu, {
+  _drawButton: function() {
+    this._super();
+    var selected = this.element
+    .find( '[selected]' )
+    .length,
+        placeholder = this.options.placeholder;
+
+    if (!selected && placeholder) {
+      this.buttonItem.text(placeholder);
+    }
+  }
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,3 @@
 import "%modules%/header/header";
-import "%modules%/footer/footer"
+import "%modules%/price/price";
+import "%modules%/footer/footer";
