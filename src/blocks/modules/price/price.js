9af6cfe923b4af35d7648b9288c885831d7e3b7e
Parchuk <sergeyparchuk1@gmail.com>
2020-02-01 21:01:06 +0200

    finished some block

--- src/blocks/modules/price/price.js
+++ src/blocks/modules/price/price.js
@@ -1,14 +1,14 @@
-console.log(2);
-$.widget( 'app.selectmenu', $.ui.selectmenu, {
-  _drawButton: function() {
-    this._super();
-    var selected = this.element
-    .find( '[selected]' )
-    .length,
-        placeholder = this.options.placeholder;
+// console.log(2);
+// $.widget( 'app.selectmenu', $.ui.selectmenu, {
+//   _drawButton: function() {
+//     this._super();
+//     var selected = this.element
+//     .find( '[selected]' )
+//     .length,
+//         placeholder = this.options.placeholder;
 
-    if (!selected && placeholder) {
-      this.buttonItem.text(placeholder);
-    }
-  }
-});
+//     if (!selected && placeholder) {
+//       this.buttonItem.text(placeholder);
+//     }
+//   }
+// });
